804a15774bcabf1b992bd87a598d7673d2ae2829
Omar Adel <omarzero1015@gmail.com>
2020-12-16 08:47:35 +0200

    add util class

---
+++ app/src/main/java/com/azapps/cis_intermediate_task/pojo/Utils.java
@@ -0,0 +1,16 @@
+package com.azapps.cis_intermediate_task.pojo;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class Utils {
+
+    public static void replaceFragments(Fragment fragment, FragmentManager fragmentManager, int container) {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                .replace(container, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+}
